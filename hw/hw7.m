78650c03e45845737991db978e55e55e9f586cf3
Juchan Lee <leejuchan@gmail.com>
2025-01-23 23:07:33 +0900

    [Add] add lec7, hw7

---
+++ hw/hw7.m
@@ -0,0 +1,21 @@
+% 아래와 같은 행렬이 되도록 for문 사용
+
+% 1 2 .. 10
+% 4 6 .. 22
+% 7 10.. 34
+% ..     ..
+% 25 34 106
+
+clear all;
+close all;
+clc;
+
+sequence = zeros(9, 10);
+
+for i = 1:10
+    for j = 1:9
+        sequence(j,i) = i + (j-1)*(i+2);
+    end
+end
+
+disp(sequence)
---
+++ lecture/lec7.m
@@ -0,0 +1,27 @@
+% 🌟 강의 목표:
+% 1. zeros() 공부하기
+% 2. for문 공부하기
+% 3. 숙제6 해설: DICOM RT Plan 파일을 읽고, 각 빔의 gantry angle의 range를 텍스트 파일로 출력하기
+
+% 🌟 주요 MATLAB 함수
+% 1. zeros()
+% 2. for문
+
+% zeros(2) == zeros(2,2)
+
+
+clear all;
+close all;
+clc;
+
+% zeros + for
+times_table_result = zeros(9,8);
+
+for j = 1:8
+    n = j + 1;
+
+    % n-times table
+    for i = 1:9
+        times_table_result(j,i) = n*i;
+    end
+end
